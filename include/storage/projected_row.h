f7664483e52ba0211b95be705fcb4709431a81f7
uchenily <uchenily@qq.com>
2024-07-12 13:51:38 +0800

    ProjectedRow

--- include/storage/projected_row.h
+++ include/storage/projected_row.h
@@ -27,8 +27,8 @@ class StorageInterface;
 namespace noisepage::storage {
 // TODO(Tianyu): To be consistent with other places, maybe move val_offset fields in front of col_ids
 /**
- * A projected row is a partial row image of a tuple. It also encodes
- * a projection list that allows for reordering of the columns. Its in-memory
+ * A projected row is a partial row image(行图像?) of a tuple. It also encodes
+ * a projection list(投影列表) that allows for reordering of the columns(对列进行重新排序). Its in-memory
  * layout:
  * -------------------------------------------------------------------------------
  * | size | num_cols | col_id1 | col_id2 | ... | val1_offset | val2_offset | ... |
@@ -37,11 +37,24 @@ namespace noisepage::storage {
  * -------------------------------------------------------------------------------
  * Warning, 0 means null in the null-bitmap
  *
- * The projection list is encoded as position of col_id -> col_id. For example:
+ * The projection list is encoded as `position of col_id` -> `col_id`. For example:
  *
  * --------------------------------------------------------
  * | 36 | 3 | 1 | 0 | 2 | 0 | 4 | 8 | 0xC0 | 721 | 15 | x |
  * --------------------------------------------------------
+ * NOTE(x):
+ * - size: 36字节
+ * - num_cols: 3列
+ * - col_id1: 1 (column IDs, 列的顺序为1)
+ * - col_id2: 0 (列顺序为0)
+ * - col_id3: 2 (列顺序为2)
+ * - val1_offset: 0 (偏移量, 表示在内存中的位置)
+ * - val2_offset: 4
+ * - val3_offset: 8
+ * - null-bitmap: 0xC0 (二进制 11000000,表示第1列和第2列非null,第3列为null)
+ * - val1: 721
+ * - val2: 15
+ * - val3: null (因为 null-bitmap 中对应位为 0)
  * Would be the row: { 0 -> 15, 1 -> 721, 2 -> nul}
  */
 // The PACKED directive here is not necessary, but C++ does some weird thing where it will pad sizeof(ProjectedRow)
@@ -63,19 +76,19 @@ public:
      * @param other ProjectedRow to use as template for setup
      * @return pointer to the initialized ProjectedRow
      */
-    static ProjectedRow *CopyProjectedRowLayout(void *head, const ProjectedRow &other);
+    static auto CopyProjectedRowLayout(void *head, const ProjectedRow &other) -> ProjectedRow *;
 
     /**
      * @return the size of this ProjectedRow in memory, in bytes
      */
-    uint32_t Size() const {
+    auto Size() const -> uint32_t {
         return size_;
     }
 
     /**
      * @return number of columns stored in the ProjectedRow
      */
-    uint16_t NumColumns() const {
+    auto NumColumns() const -> uint16_t {
         return num_cols_;
     }
 
@@ -83,7 +96,7 @@ public:
      * @warning don't use these above the storage layer, they have no meaning
      * @return pointer to the start of the array of column ids
      */
-    col_id_t *ColumnIds() {
+    auto ColumnIds() -> col_id_t * {
         return reinterpret_cast<col_id_t *>(varlen_contents_);
     }
 
@@ -91,7 +104,7 @@ public:
      * @warning don't use these above the storage layer, they have no meaning
      * @return pointer to the start of the array of column ids
      */
-    const col_id_t *ColumnIds() const {
+    auto ColumnIds() const -> const col_id_t * {
         return reinterpret_cast<const col_id_t *>(varlen_contents_);
     }
 
@@ -101,10 +114,11 @@ public:
      * @return byte pointer to the attribute. reinterpret_cast and dereference to access the value. if attribute is
      * nullable and set to null, then return value is nullptr
      */
-    byte *AccessWithNullCheck(const uint16_t offset) {
+    auto AccessWithNullCheck(const uint16_t offset) -> byte * {
         NOISEPAGE_ASSERT(offset < num_cols_, "Column offset out of bounds.");
-        if (!Bitmap().Test(offset))
+        if (!Bitmap().Test(offset)) {
             return nullptr;
+        }
         return reinterpret_cast<byte *>(this) + AttrValueOffsets()[offset];
     }
 
@@ -114,10 +128,11 @@ public:
      * @return byte pointer to the attribute. reinterpret_cast and dereference to access the value. if attribute is
      * nullable and set to null, then return value is nullptr
      */
-    const byte *AccessWithNullCheck(const uint16_t offset) const {
+    auto AccessWithNullCheck(const uint16_t offset) const -> const byte * {
         NOISEPAGE_ASSERT(offset < num_cols_, "Column offset out of bounds.");
-        if (!Bitmap().Test(offset))
+        if (!Bitmap().Test(offset)) {
             return nullptr;
+        }
         return reinterpret_cast<const byte *>(this) + AttrValueOffsets()[offset];
     }
 
@@ -126,10 +141,11 @@ public:
      * @param offset The 0-indexed element to access in this ProjectedRow
      * @return byte pointer to the attribute. reinterpret_cast and dereference to access the value
      */
-    byte *AccessForceNotNull(const uint16_t offset) {
+    auto AccessForceNotNull(const uint16_t offset) -> byte * {
         NOISEPAGE_ASSERT(offset < num_cols_, "Column offset out of bounds.");
-        if (!Bitmap().Test(offset))
+        if (!Bitmap().Test(offset)) {
             Bitmap().Flip(offset);
+        }
         return reinterpret_cast<byte *>(this) + AttrValueOffsets()[offset];
     }
 
@@ -156,7 +172,7 @@ public:
      * @param offset The 0-indexed element to access in this ProjectedRow
      * @return true if null, false otherwise
      */
-    bool IsNull(const uint16_t offset) const {
+    auto IsNull(const uint16_t offset) const -> bool {
         NOISEPAGE_ASSERT(offset < num_cols_, "Column offset out of bounds.");
         return !Bitmap().Test(offset);
     }
@@ -170,7 +186,7 @@ public:
      * @return The typed value at the current iterator position in the column
      */
     template <typename T, bool Nullable>
-    const T *Get(const uint16_t col_idx, bool *const null) const {
+    auto Get(const uint16_t col_idx, bool *const null) const -> const T * {
         const auto *result = reinterpret_cast<const T *>(AccessWithNullCheck(col_idx));
         // NOLINTNEXTLINE: bugprone-suspicious-semicolon: seems like a false positive because of constexpr
         if constexpr (Nullable) {
@@ -212,19 +228,19 @@ private:
     uint16_t num_cols_;
     byte     varlen_contents_[0];
 
-    uint32_t *AttrValueOffsets() {
+    auto AttrValueOffsets() -> uint32_t * {
         return StorageUtil::AlignedPtr<uint32_t>(ColumnIds() + num_cols_);
     }
 
-    const uint32_t *AttrValueOffsets() const {
+    auto AttrValueOffsets() const -> const uint32_t * {
         return StorageUtil::AlignedPtr<const uint32_t>(ColumnIds() + num_cols_);
     }
 
-    common::RawBitmap &Bitmap() {
+    auto Bitmap() -> common::RawBitmap & {
         return *reinterpret_cast<common::RawBitmap *>(AttrValueOffsets() + num_cols_);
     }
 
-    const common::RawBitmap &Bitmap() const {
+    auto Bitmap() const -> const common::RawBitmap & {
         return *reinterpret_cast<const common::RawBitmap *>(AttrValueOffsets() + num_cols_);
     }
 };
@@ -246,26 +262,26 @@ public:
      * @param head pointer to the byte buffer to initialize as a ProjectedRow
      * @return pointer to the initialized ProjectedRow
      */
-    ProjectedRow *InitializeRow(void *head) const;
+    auto InitializeRow(void *head) const -> ProjectedRow *;
 
     /**
      * @return size of the ProjectedRow in memory, in bytes, that this initializer constructs.
      */
-    uint32_t ProjectedRowSize() const {
+    auto ProjectedRowSize() const -> uint32_t {
         return size_;
     }
 
     /**
      * @return number of columns in the projection list
      */
-    uint16_t NumColumns() const {
+    auto NumColumns() const -> uint16_t {
         return static_cast<uint16_t>(col_ids_.size());
     }
 
     /**
      * @return column ids at the given offset in the projection list
      */
-    col_id_t ColId(uint16_t i) const {
+    auto ColId(uint16_t i) const -> col_id_t {
         return col_ids_.at(i);
     }
 
@@ -282,7 +298,7 @@ public:
      * @param layout BlockLayout of the RawBlock to be accessed
      * @param col_ids projection list of column ids to map, should have all unique values (no repeats)
      */
-    static ProjectedRowInitializer Create(const BlockLayout &layout, std::vector<col_id_t> col_ids);
+    static auto Create(const BlockLayout &layout, std::vector<col_id_t> col_ids) -> ProjectedRowInitializer;
 
     /**
      * Constructs a ProjectedRowInitializer. Calculates the size of this ProjectedRow, including all members, values,
@@ -292,8 +308,8 @@ public:
      * @param real_attr_sizes unsorted REAL attribute sizes, e.g. they shouldn't use MSB to indicate varlen.
      * @param pr_offsets pr_offsets[i] = projection list offset of attr_sizes[i] after it gets sorted
      */
-    static ProjectedRowInitializer Create(std::vector<uint16_t>        real_attr_sizes,
-                                          const std::vector<uint16_t> &pr_offsets);
+    static auto Create(std::vector<uint16_t> real_attr_sizes, const std::vector<uint16_t> &pr_offsets)
+        -> ProjectedRowInitializer;
 
 private:
     friend class catalog::Catalog;                   // access to the PRI default constructor
@@ -315,8 +331,8 @@ private:
      * @param real_attr_sizes unsorted REAL attribute sizes, e.g. they shouldn't use MSB to indicate varlen.
      * @param col_ids column ids
      */
-    static ProjectedRowInitializer Create(const std::vector<uint16_t> &real_attr_sizes,
-                                          const std::vector<col_id_t> &col_ids);
+    static auto Create(const std::vector<uint16_t> &real_attr_sizes, const std::vector<col_id_t> &col_ids)
+        -> ProjectedRowInitializer;
 
     /**
      * Constructs a ProjectedRowInitializer. Calculates the size of this ProjectedRow, including all members, values,
